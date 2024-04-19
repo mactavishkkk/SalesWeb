6458b02833b8a4a3ffac0d7d0e448d15d3818f31
isaías <isaiasleite230@gmail.com>
2024-04-18 23:11:05 -0300

    adição do método details

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -22,6 +22,23 @@ namespace SalesWeb.Controllers
             return View(sellers);
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWeb.Data;
 using SalesWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWeb.Services
 {
@@ -25,7 +26,7 @@ namespace SalesWeb.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
